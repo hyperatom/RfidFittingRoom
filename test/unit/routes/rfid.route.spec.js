4d2c62e77bbec5e32713015b9d03378c17db34fd
Adam Barrell <a.j.barrell@gmail.com>
2015-04-13 17:20:10 +0100

    Tested loading of nested related products.

--- test/unit/routes/rfid.route.spec.js
+++ test/unit/routes/rfid.route.spec.js
@@ -2,9 +2,12 @@ var req = require('supertest-as-promised'),
   sinon = require('sinon'),
   expect = require('chai').expect,
   sails = require('sails'),
-  testImages = require('../../helpers/images');
+  testProducts = require('../../helpers/products'),
+  productTester = require('../../helpers/productTester');
 
 describe('RFID Route Availability', function() {
+
+  this.timeout(5000);
 
   describe('POST /rfid', function() {
 
@@ -46,35 +49,44 @@ describe('RFID Route Availability', function() {
           expect(res.body).to.have.property('description');
           expect(res.body).to.have.property('price');
           expect(res.body).to.have.property('image');
+          expect(res.body).to.have.property('relatedProducts');
+          done();
+        });
+    });
+
+    it('should have nested products', function(done) {
+
+      req(sails.hooks.http.app)
+        .get('/rfid')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          expect(res.body).to.have.property('id');
+          expect(res.body).to.have.property('name');
+          expect(res.body).to.have.property('description');
+          expect(res.body).to.have.property('price');
+          expect(res.body).to.have.property('image');
+          expect(res.body).to.have.property('relatedProducts');
           done();
         });
     });
 
   });
-
 });
 
 describe('RFID-Product mappings', function() {
 
+  this.timeout(5000);
+
   it('should return the correct product for RFID code 11111', function(done) {
 
-    function assertCorrectProduct(done) {
+    function getActiveProduct(done) {
 
       return req(sails.hooks.http.app)
         .get('/rfid')
         .expect(200)
         .end(function(err, res) {
-
-          expect(res.body.id).to.equal(11111);
-          expect(res.body.name).to.equal('Ruched Neckline Vest Shift Dress');
-
-          expect(res.body.description).to.equal(
-            'Dressing down for the beach doesn\'t mean a ' +
-            'compromise in style; this beach dress is a cute option.');
-
-          expect(res.body.price).to.equal(16.00);
-          expect(res.body.image).to.equal(testImages.dress_1);
-
+          productTester.assert(res.body, testProducts.product_1);
           done();
         });
     }
@@ -84,29 +96,19 @@ describe('RFID-Product mappings', function() {
       .send({id: 11111})
       .expect(200)
       .end(function() {
-        assertCorrectProduct(done);
+        getActiveProduct(done);
       });
   });
 
   it('should return the correct product for RFID code 22222', function(done) {
 
-    function assertCorrectProduct(done) {
+    function getActiveProduct(done) {
 
       return req(sails.hooks.http.app)
         .get('/rfid')
         .expect(200)
         .end(function(err, res) {
-
-          expect(res.body.id).to.equal(22222);
-          expect(res.body.name).to.equal('Floral Fit & Flare Tea Dress');
-
-          expect(res.body.description).to.equal(
-            'For a gorgeously ladylike look, this tea dress is just the thing. ' +
-            'Floral prints are an easy way to channel a current catwalk trend.');
-
-          expect(res.body.price).to.equal(39.50);
-          expect(res.body.image).to.equal(testImages.dress_2);
-
+          productTester.assert(res.body, testProducts.product_2);
           done();
         });
     }
@@ -116,8 +118,7 @@ describe('RFID-Product mappings', function() {
       .send({id: 22222})
       .expect(200)
       .end(function() {
-        assertCorrectProduct(done);
+        getActiveProduct(done);
       });
   });
-
 });
