29122a093d98347af36697e21a60c683a59ac616
Adam Barrell <a.j.barrell@gmail.com>
2015-04-14 15:18:59 +0100

    Using monolab database for production environment.

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  mongoDb: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    host: 'ds039351.mongolab.com',
+    port: 39351,
+    user: 'rfid-fitting-room-client',
+    password: 'marksandspencer',
+    database: 'rfid-fitting-room-db'
   },
 
   /***************************************************************************
